dd1c7ae832345fb7a4a4f9bda6f775a956f757fc
Mike Perham <mperham@gmail.com>
2010-11-20 21:30:49 -0800

    Remove old AR2.1 test application

--- example/app/controllers/figments_controller.rb
+++
@@ -1,8 +0,0 @@
-class FigmentsController < ApplicationController
-  
-  def create
-    @account.figments.create!(params[:figment])
-    flash[:notice] = "Figment created"
-    redirect_to '/'
-  end
-end
